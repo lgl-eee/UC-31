6161627088e993085e2e20ed713604a9cd202229
pedro jewur <84276531+lgl-eee@users.noreply.github.com>
2026-06-08 16:08:58 -0300

    Add files via upload

---
+++ UC31 08-06-26/templates/inicio.py
@@ -0,0 +1,41 @@
+from flask import(
+    Flask,
+    render_template,
+    request,
+    make_response,
+    redirect,
+    url_for
+)
+
+app = Flask(__name__)
+
+@app.route('/')
+def inicio():
+    
+    #lê o cookie
+    tema = request.cookies.get('tema', 'claro')
+
+    return render_template(
+        inicio.html,
+        tema=tema
+    )
+
+@app.route('/tema/<escolha>')
+def trocar_tema(escolha):
+
+    if escolha not in ['claro', 'escuro']:
+        escolha = 'claro'
+
+    resposta = make_response(
+        redirect(url_for('inicio'))
+    )
+
+    resposta.set_cookie(
+        'tema',
+        escolha,
+        max_age=60*60*24*30    
+    )
+
+    return resposta
+if __name__ == '__main__':
+    app.run(debug=True)
